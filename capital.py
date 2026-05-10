7628b81396db31066b0d8b749ab29c3b47c44d81
tarun05-design <tarunparthasarathy65@gmail.com>
2026-05-10 12:04:17 +0530

    Add capitalize solution

---
+++ capital.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    words= s.split(" ")
+    text=[word.capitalize() for word in words]
+    return " ".join(text)
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
